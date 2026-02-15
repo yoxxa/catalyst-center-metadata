5ad6dfb34c2eef90336b165c3f65c49ecb26317f
yoxxa <theyoxxa@protonmail.com>
2026-02-15 13:02:53 +0800

    feat: add `Device` collection mechanism

--- src/collector/__init__.py
+++ src/collector/__init__.py
@@ -1,10 +1,20 @@
 from bcs_oi_api import BCSOIAPI
-
+from bcs_oi_api.models import (
+    Device,
+    Asset, 
+    Contract
+)
 class BCSOICollector:
     def __init__(self, client_id, client_secret, region) -> None:
-        self.bcs_sdk = bcs_oi_api = BCSOIAPI(
+        self.bcs_sdk = BCSOIAPI(
             client_id = client_id,
             client_secret = client_secret,
             region = region
         )
         self.data: list[dict] = list()
+
+    def collect_data(self) -> None:
+        self.data.extend(self.get_devices())
+
+    def get_devices(self) -> list[dict]:
+        return [dict(device) for device in self.bcs_sdk.get_output(model = Device)]
